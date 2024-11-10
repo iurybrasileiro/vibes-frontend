c248f0813eb5959d182f79992aef94f574a556a8
Iury Brasileiro <Iury.brasileiro@outlook.com.br>
2024-11-10 20:17:29 -0300

    chore: initialized project

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app.config.server';
+import { ApplicationRef } from '@angular/core'
+import { bootstrapApplication } from '@angular/platform-browser'
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+import { AppComponent } from './app/app.component'
+import { config } from './app/app.config.server'
 
-export default bootstrap;
+const bootstrap = async (): Promise<ApplicationRef> =>
+  await bootstrapApplication(AppComponent, config)
+
+export default bootstrap
